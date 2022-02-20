50dd3d8cc47c1f7ca1c7b0add45d7bb3c37ecbff
Bryan Crause <bcrause@hotmail.es>
2022-02-20 20:54:41 -0300

    Creación de rutas admin Daniel Salas

--- Blog/AppBlog/admin.py
+++ Blog/AppBlog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from AppBlog.models import Avatar
+from AppBlog.models import Artista, Avatar, Cliente, Galeria
 
 admin.site.register(Avatar)
-
-
+admin.site.register(Cliente)
+admin.site.register(Artista)
+admin.site.register(Galeria)
